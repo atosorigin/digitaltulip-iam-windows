cbd3e6e39a31905124d6ab327e86c196382c12fb
Lee Gathercole <lee@fathomatics.com>
2015-04-29 15:42:14 +0100

    Purge

--- roles/activedirectory/files/add_user.ps1
+++ roles/activedirectory/files/add_user.ps1
@@ -1,11 +1,10 @@
-param([string]$firstname = "",[string]$surname="", [string]$email="",  [string]$idpfqdn="")
+param([string]$firstname = "",[string]$surname="", [string]$email="",  [string]$idpfqdn="", [string]$initialpassword="")
 
 Import-Module activedirectory
 
 $Displayname = $firstname + " " + $surname            
 $SamAccountName    = $firstname.ToLower() + "." + $surname.ToLower()           
 $UserPrincipalName = $email  
-$initialpassword = "Tul1p1234"
 $sunfmsaml2nameidinfokey  = "https://" + $idpfqdn + "/openam|urn:federation:MicrosoftOnline|" +  $SamAccountName
 $sunfmsaml2nameidinfo  = "https://" + $idpfqdn +  "/openam|urn:federation:MicrosoftOnline|" + $SamAccountName + "|https://" + $idpfqdn + "/openam|urn:oasis:names:tc:SAML:2.0:nameid-format:persistent|"  +  $SamAccountName + "|urn:federation:MicrosoftOnline|IDPRole|false"
 
